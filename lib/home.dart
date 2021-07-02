5eaa98f782c2aa14f3e4c653330d0fb0dc7c3749
newchetsada <new_chetsada@hotmail.com>
2021-07-02 20:05:19 +0700

    ขยายรูป

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 
-
-
 class home extends StatefulWidget {
   @override
   _homeState createState() => _homeState();
 }
 
 class _homeState extends State<home> {
-
   Widget showTextLogo() {
     return Container(
       margin: EdgeInsets.only(top: 50.0),
@@ -41,91 +38,93 @@ class _homeState extends State<home> {
     );
   }
 
-  Widget showPic(){
+  Widget showPic() {
     return Row(
       mainAxisSize: MainAxisSize.min,
-      children: [
-        showLogin()
-      ],
+      children: [showLogin()],
     );
   }
 
   Widget showLogin() {
-    return 
-        Image.asset("assets/login1.png",
-        width: 200,
-        height: 300,);
-    
+    return Image.asset(
+      "assets/login1.png",
+      width: 300,
+      height: 400,
+    );
   }
 
-  Widget showButton(){
+  Widget showButton() {
     return Column(
       children: [
-       showButton1 (),
-       SizedBox(
-        height: 50.0,),
-       showButton2 ()
+        showButton1(),
+        SizedBox(
+          height: 50.0,
+        ),
+        showButton2()
       ],
     );
   }
 
-  Widget showButton1 () {
+  Widget showButton1() {
     return SizedBox(
       width: 300,
       height: 50,
       child: RaisedButton(
-        child: Text("เข้าสู่ระบบ",
-        style: TextStyle(
-          fontFamily: "MPLUSRounded1c",
-          color: Color(0xFF707070),
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-        ),) ,
+        child: Text(
+          "เข้าสู่ระบบ",
+          style: TextStyle(
+            fontFamily: "MPLUSRounded1c",
+            color: Color(0xFF707070),
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         color: Color(0xFFE5EFC1),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
-        onPressed: (){},
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30))),
+        onPressed: () {},
       ),
     );
   }
 
-  Widget showButton2 (){
+  Widget showButton2() {
     return SizedBox(
       width: 300,
       height: 50,
       child: RaisedButton(
-        child: Text("ลงทะเบียน",
-        style: TextStyle(
-          fontFamily: "MPLUSRounded1c",
-          color: Color(0xFF707070),
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-        ),) ,
+        child: Text(
+          "ลงทะเบียน",
+          style: TextStyle(
+            fontFamily: "MPLUSRounded1c",
+            color: Color(0xFF707070),
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         color: Color(0xFFE5EFC1),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
-        onPressed: (){},
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30))),
+        onPressed: () {},
       ),
     );
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF557B83),
-        body: Center( 
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              showTextLogo(),
-              showTextLogo2(),
-              showPic(),
-              showButton()
-            ],),
-          
-        ) 
-      ),
+          backgroundColor: Color(0xFF557B83),
+          body: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                showTextLogo(),
+                showTextLogo2(),
+                showPic(),
+                showButton()
+              ],
+            ),
+          )),
     );
   }
 }
