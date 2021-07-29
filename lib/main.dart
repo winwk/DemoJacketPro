b72f40d7b132592cc90fb410a3e7846ccec9b8e1
newchetsada <new_chetsada@hotmail.com>
2021-07-29 18:21:00 +0700

    Revert "เชื่อม firebase , Add Plugin Firebase"
    This reverts commit a7664b95833731d9de299be19f7e53f13d59f734.

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: JacketPro(),
+      home: home1(),
       routes: {
         Register_Screen.route: (context) => Register_Screen(),
       },
