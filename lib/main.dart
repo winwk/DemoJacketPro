a7664b95833731d9de299be19f7e53f13d59f734
winwk <weeraya448@gmail.com>
2021-07-29 16:01:43 +0700

    เชื่อม firebase , Add Plugin Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: home1(),
+      home: JacketPro(),
       routes: {
         Register_Screen.route: (context) => Register_Screen(),
       },
